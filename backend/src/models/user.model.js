8f385ca6779775140495f82dd1e89b9d37597494
Botond Kulcsar <bkulcsar@gmx.com>
2021-08-25 13:57:49 +0300

    nickName added to User model

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -9,6 +9,10 @@ const UserSchema = new mongoose.Schema({
         type: String,
         required: true
     },
+    nickName: {
+        type: String,
+        required: true
+    },
     city: {
         type: String,
         required: true
