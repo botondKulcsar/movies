43a6c59602fbae8f279dacb60fc05e9d88d79786
Botond Kulcsar <bkulcsar@gmx.com>
2021-09-02 01:55:17 +0300

    getAllDemoMovies promisified :)

--- backend/src/controllers/movie/movie.controller.js
+++ backend/src/controllers/movie/movie.controller.js
@@ -4,17 +4,14 @@ const createError = require('http-errors');
 
 exports.getDemoMovies = async (req, res, next) => {
     try {
-        await movieService.getAllDemoMovies(data => {
-            movieList = data;
-            if (!movieList) {
-                throw new Error('could not read sample data')
-            }
-            res.status(200);
-            res.json(movieList);
-        });
-        
-       
-    } catch (error) {
+        const movieList = await movieService.getAllDemoMovies()
+        if (!movieList) {
+            throw new Error('could not read sample data')
+        }
+        res.status(200);
+        res.json(movieList);
+    }
+    catch (error) {
         next(new createError.InternalServerError(error.message));
     }
 }
--- backend/src/controllers/movie/movie.service.js
+++ backend/src/controllers/movie/movie.service.js
@@ -4,15 +4,19 @@ const path = require('path')
 
 
 exports.getAllDemoMovies =  (cb) => 
-    readFile(path.join(__dirname, '../../demo-data/movies.json'), 'utf-8', (err, data) => {
-        if (err) {
-            throw new Error('could not read json file')
-        } else {
-            const parsedData = JSON.parse(data);
-            movies = parsedData.items;
-            return cb(movies);
-        }
-    });
+
+    new Promise((res, rej) => {
+        readFile(path.join(__dirname, '../../demo-data/movies.json'), 'utf-8', (err, data) => {
+            if (err) {
+                rej(err)
+            } else {
+                const parsedData = JSON.parse(data);
+                movies = parsedData.items;
+                res(movies);
+            }
+        });
+    })
+    
     
     
 
