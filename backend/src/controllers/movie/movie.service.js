330cec329178ddda02386faf17f57f44505e3d53
Botond Kulcsar <bkulcsar@gmx.com>
2021-09-02 13:59:38 +0300

    GET /api/movies PATCH /api/movies/:id FROM mongoDB

--- backend/src/controllers/movie/movie.service.js
+++ backend/src/controllers/movie/movie.service.js
@@ -1,21 +1,27 @@
 
-const { readFile } = require('fs');
-const path = require('path')
+// const { readFile } = require('fs');
+// const path = require('path')
 
 
-exports.getAllDemoMovies =  (cb) => 
+// exports.getAllDemoMovies =  (cb) => 
 
-    new Promise((res, rej) => {
-        readFile(path.join(__dirname, '../../demo-data/movies.json'), 'utf-8', (err, data) => {
-            if (err) {
-                rej(err)
-            } else {
-                const parsedData = JSON.parse(data);
-                movies = parsedData.items;
-                res(movies);
-            }
-        });
-    })
+//     new Promise((res, rej) => {
+//         readFile(path.join(__dirname, '../../demo-data/movies.json'), 'utf-8', (err, data) => {
+//             if (err) {
+//                 rej(err)
+//             } else {
+//                 const parsedData = JSON.parse(data);
+//                 movies = parsedData.items;
+//                 res(movies);
+//             }
+//         });
+//     })
+
+const Movie = require('../../models/movie.model')
+
+exports.getTop250Movies = () => Movie.find();
+
+exports.findMovieByIdAndUpdate = (id, payload) => Movie.findByIdAndUpdate(id, payload, { new: true })
     
     
     
